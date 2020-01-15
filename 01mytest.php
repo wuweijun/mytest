57a149a0585ce446450e3af781801ce9c265e8ee
1052056847@qq.com <1052056847@qq.com>
2020-01-15 11:52:34 +0800

    学习成员变量，方法
    学习成员变量，方法

--- 01mytest.php
+++ 01mytest.php
@@ -2,25 +2,34 @@
 	/**
 	 * 
 	 */
-	class Test
+	class Buyer
 	{
 		
-		function __construct(argument)
+		public $name;
+		public $money = 0;
+		public function ShowClassName()
 		{
-			# code...
+			echo __CLASS__;
 		}
 	}
 
-	/**
-	 * 
-	 */
-	class MyClass 
-	{
-		
-		function name($name)
-		{
-			# code...
-			echo "我的名字是".$name;
-		}
-	}
+	//实例化：对象保存在堆区
+	$Buyer1 = new Buyer();
+	//var_dump($Buyer1);
+	
+	//访问属性（数据）
+	echo $Buyer1->money;
+
+	//修改属性（数据）
+	$Buyer1->name = '吴伟俊';
+	echo $Buyer1->name;
+
+	echo '<hr/>';
+
+	//访问成员方法
+	echo $Buyer1->ShowClassName();
+
+
+
+	
  ?>
