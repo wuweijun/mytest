233101ec80b1f4b008f6d5f8ad84dc9bf29754f2
1052056847@qq.com <1052056847@qq.com>
2020-01-15 11:13:11 +0800

    添加文件
    添加文件

---
+++ 01mytest.php
@@ -0,0 +1,26 @@
+<?php 
+	/**
+	 * 
+	 */
+	class Test
+	{
+		
+		function __construct(argument)
+		{
+			# code...
+		}
+	}
+
+	/**
+	 * 
+	 */
+	class MyClass 
+	{
+		
+		function name($name)
+		{
+			# code...
+			echo "我的名字是".$name;
+		}
+	}
+ ?>
